26dddbbf80b1bd70fb31ccc0a2356d5f23a32552
unknown <IT22592156@mt.sliit.lk>
2025-10-11 01:06:05 +0530

    Initial commit

--- config/firebase.ts
+++ config/firebase.ts
@@ -16,24 +16,14 @@ type FirebaseConfig = {
 
 const extra = (Constants.expoConfig?.extra ?? {}) as { firebase?: Partial<FirebaseConfig> };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyClVKFpjid-dCfw4Oqx-0A2rY_NUFtJPgc",
-  authDomain: "gameproject-f345b.firebaseapp.com",
-  projectId: "gameproject-f345b",
-  storageBucket: "gameproject-f345b.firebasestorage.app",
-  messagingSenderId: "793337624143",
-  appId: "1:793337624143:web:e4d42cffcb1559971a5c3a",
-  measurementId: "G-WZBHSNWN1J"
+const firebaseConfig: FirebaseConfig = {
+  apiKey: extra.firebase?.apiKey ?? '',
+  authDomain: extra.firebase?.authDomain ?? '',
+  projectId: extra.firebase?.projectId ?? '',
+  storageBucket: extra.firebase?.storageBucket ?? '',
+  messagingSenderId: extra.firebase?.messagingSenderId ?? '',
+  appId: extra.firebase?.appId ?? '',
 };
-
-// const firebaseConfig: FirebaseConfig = {
-//   apiKey: extra.firebase?.apiKey ?? "AIzaSyClVKFpjid-dCfw4Oqx-0A2rY_NUFtJPgc",
-//   authDomain: extra.firebase?.authDomain ?? "gameproject-f345b.firebaseapp.com",
-//   projectId: extra.firebase?.projectId ?? "gameproject-f345b",
-//   storageBucket: extra.firebase?.storageBucket ?? "gameproject-f345b.firebasestorage.app",
-//   messagingSenderId: extra.firebase?.messagingSenderId ?? "793337624143",
-//   appId: extra.firebase?.appId ?? "1:793337624143:web:e4d42cffcb1559971a5c3a"
-// };
 const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 // Initialize Auth with React Native AsyncStorage persistence
